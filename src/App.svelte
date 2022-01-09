b34edcba5acadcc174abc6ef6933f2b87b4b3a03
Olivér <oliver.mrakovics@gmail.com>
2022-01-09 14:09:21 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import FirstForm from "./FirstForm.svelte";
+</script>
+
+<main class="container">
+	<div class="row justify-content-center my-4">
+		<div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-6">
+			<h2 class="display-6">VLSM calculator</h2>
+			<hr>
+		</div>
+	</div>
+	<div class="row justify-content-center">
+		<div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-6">
+			<FirstForm />
+		</div>
+	</div>
+</main>
+
+<style>
+	.display-6 {
+		text-align: center;
+	}
+</style>
---
+++ src/FirstForm.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<main>
+    <div class="card">
+        <div class="card-body">
+            <div class="mb-4">
+                <label for="subnets" class="form-label">How many subnets do you need?</label>
+                <div class="row">
+                    <div class="m-0 col-1"><button class="btn btn-danger w-100">-</button></div>
+                    <div class="m-0 col-10"><input type="number" id="subnets" class="form-control" value="2" disabled></div>
+                    <div class="m-0 col-1"><button class="btn btn-success w-100">+</button></div>
+                </div>
+            </div>
+
+            <hr class="my-3">
+
+            <div class="my-4">
+                <label for="ip" class="form-label">Base IP Address</label>
+                <div class="input-group">
+                    <input id="ip" type="text" class="form-control" placeholder="192.168.0.1">
+                    <span class="input-group-text">/24</span>
+                </div>
+            </div>
+            
+            <button class="btn btn-primary">NEXT</button>
+        </div>
+    </div>
+</main>
+
+<style>
+    .w-100 {
+        width: 100%;
+        font-weight: 500;
+    } 
+</style>
